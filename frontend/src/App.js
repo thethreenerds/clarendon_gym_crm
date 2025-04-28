04d7fd3dea3a9ae1d9ec56cba33dbb2ab09da670
Kealan <kabrahambusse@gmail.com>
2025-04-28 14:38:00 +0100

    created frontend files with react, deleted unnessary files.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return(
+    <div>
+      <h1>Clarendon Gym CRM</h1>
+      <p>Welcome to the System!</p>
+    </div>
+  );
+}
+
+export default App;
