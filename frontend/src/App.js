9573c5bd8d221a4eff21a58f69227c02b9abee37
Kealan <kabrahambusse@gmail.com>
2025-05-04 13:10:27 +0100

    compiler error fix return outside of scope.  file naming error on import.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import CreateUserForm from './components/createuserform';
+import CreateUserForm from './components/CreateUserForm';
 
 function App() {
   return(
     <div>
       <h1>Clarendon Gym CRM</h1>
       <p>Welcome to the System!</p>
+
+      <CreateUserForm/>
     </div>
   );
 }
--- frontend/src/components/CreateUserForm.js
+++ frontend/src/components/CreateUserForm.js
@@ -7,27 +7,27 @@ function CreateUserForm(){
         password: "",
         role: ""
     });
+
+
+    const handleChange = (e) => {
+        setFormData({...formDatam, [e.target.name]: e.target.value});
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Create New User</h2>
+            <input name="name" placeholder="Name" value ={formData.name} onChange={handleChange}/>
+            <input name="email" placeholder="Email" value ={formData.email} onChange={handleChange}/>
+            <input name="password" placeholder="Password" value ={formData.password} onChange={handleChange}/>
+            <input name="role" placeholder="Role" value ={formData.role} onChange={handleChange}/>
+            <button type ="submit">Create User</button>
+        </form>
+
+    );
 }
-
-const handleChange = (e) => {
-    setFormData({...formDatam, [e.target.name]: e.target.value});
-};
-
-const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(formData);
-};
-
-return (
-    <form onSubmit={handleSubmit}>
-        <h2>Create New User</h2>
-        <input name="name" placeholder="Name" value ={formData.name} onChange={handleChange}/>
-        <input name="email" placeholder="Email" value ={formData.email} onChange={handleChange}/>
-        <input name="password" placeholder="Password" value ={formData.password} onChange={handleChange}/>
-        <input name="role" placeholder="Role" value ={formData.role} onChange={handleChange}/>
-        <button type ="submit">Create User</button>
-    </form>
-
-);
-
-export default CreateUserForm;
+    export default CreateUserForm;
